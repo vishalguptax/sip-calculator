3398e6dca2868e901340efc6f49dcb74e4b05033
Vishal <vslmsg@gmail.com>
2021-12-20 16:13:22 +0530

    share button

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,24 @@
 if (window.history.replaceState) {
     window.history.replaceState(null, null, window.location.href);
 }
+
 const shareData = {
-    title: 'SIP Calculator',
-    text: 'Check out this easy Mutual funds SIP Calculator!',
-    url: 'http://sipcalculator.herokuapp.com/'
+    title: 'MDN',
+    text: 'Learn web development on MDN!',
+    url: 'https://developer.mozilla.org'
 }
 
 const btn = document.querySelector('.share');
-btn.addEventListener('click', () => {
-    navigator.share(shareData);
-});
+
+// Share must be triggered by "user activation"
+
+window.onload = function () {
+    btn.addEventListener('click', async () => {
+        try {
+            await navigator.share(shareData)
+            btn.textContent = 'Shared ✅'
+        } catch (err) {
+            btn.textContent = 'Error: ' + err
+        }
+    });
+}
